94f9919fdb3dfb68b8f02d7eeb55bf6ed5aa0cdb
Sagor8187 <sdsagor8187@gmail.com>
2026-05-01 10:13:06 +0600

    navbar and header done

--- src/component/shared/Header.jsx
+++ src/component/shared/Header.jsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import logo from "@/assets/logo.png"
+import { format } from "date-fns";
 export default function Header() {
   return (
     <div>
-      <div>
-         <Image 
+      <div className="text-center p-10 space-y-3">
+         <Image  className="mx-auto"
         src={logo}
         alt=""
         width={300}
         height={200}
       />
-      <h2>Journalism Without Fear or Favour</h2>
+      <h2 className="text-gray-500">Journalism Without Fear or Favour</h2>
+      <p className="text-gray-500 font-bold ">
+{format(new Date(), "EEEE, MMMM dd, yyyy")}
+      </p>
+      
       </div>
     </div>
   )
--- src/component/shared/Navbar.jsx
+++ src/component/shared/Navbar.jsx
@@ -1,10 +1,39 @@
-
 export default function Navbar() {
   return (
     <div>
-     <div>
-        <h2>navbar</h2>
-     </div>
+      <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a>Home</a></li>
+        <li><a>About</a></li>
+        <li><a>Career</a></li>
+      </ul>
     </div>
-  )
+    <a className="btn btn-ghost text-xl"></a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1 text-gray-500 font-bold">
+     <li><a>Home</a></li>
+        <li><a>About</a></li>
+        <li><a>Career</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end flex gap-3">
+    <div className="w-10 rounded-full">
+          <img className="w-10 rounded-full"
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+
+    <a className="btn">Login</a>
+  </div>
+</div>
+    </div>
+  );
 }
