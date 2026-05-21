ee0aa24e8aee002cad10db753a90e7bb4918d0d1
Sagor8187 <sdsagor8187@gmail.com>
2026-05-21 19:12:50 +0600

    update my code

--- src/component/shared/Header.jsx
+++ src/component/shared/Header.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import logo from "@/assets/logo.png"
 import { format } from "date-fns";
 import Merque from "./Merque";
@@ -6,12 +6,7 @@ export default function Header() {
   return (
     <div>
       <div className="text-center p-10 space-y-3">
-         <Image  className="mx-auto"
-        src={logo}
-        alt=""
-        width={300}
-        height={200}
-      />
+        <h1 className="font-bold text-2xl">Global <span className="text-purple-600 text-lg">Times News</span></h1>
       <h2 className="text-gray-500">Journalism Without Fear or Favour</h2>
       <p className="text-gray-500 font-bold ">
 {format(new Date(), "EEEE, MMMM dd, yyyy")}
--- src/component/shared/Navbar.jsx
+++ src/component/shared/Navbar.jsx
@@ -1,64 +1,90 @@
-"use client"
-import { authClient } from "@/lib/auth-client";
+"use client";
+
 import Navlink from "./Navlink";
 import { RxAvatar } from "react-icons/rx";
-import Image from "next/image";
+import { authClient } from "@/lib/auth-client";
 
-export default  function Navbar() {
+export default function Navbar() {
 
+  const { data, isLoading } = authClient.useSession();
+  const info = data?.user;
 
-const { data: session } =  authClient.useSession()
-const info = session?.user
-console.log(session)
+  console.log("FULL DATA:", data);
+  console.log("USER:", info);
 
-  
+ 
+  if (isLoading) {
+    return <p className="text-center">Loading...</p>;
+  }
+
   return (
     <div>
       <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+
+       
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+
+            <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box mt-3 w-52 p-2 shadow">
+              <Navlink href={"/"}>Home</Navlink>
+              <Navlink href={"/about"}>About</Navlink>
+              <Navlink href={"/career"}>Career</Navlink>
+            </ul>
+          </div>
+        </div>
+
+        {/* CENTER */}
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-gray-500 font-bold flex gap-5">
+            <Navlink href={"/"}>Home</Navlink>
+            <Navlink href={"/about"}>About</Navlink>
+            <Navlink href={"/career"}>Career</Navlink>
+          </ul>
+        </div>
+
+        {/* RIGHT */}
+        {info ? (
+          <div className="navbar-end flex gap-3 items-center">
+
+            <h2 className="font-bold text-pink-700">
+              Welcome {info?.name}
+            </h2>
+
+            <div className="w-10 h-10">
+              {info?.image ? (
+                <img
+                  className="rounded-full w-[40px] h-[40px] object-cover"
+                  src={info.image}
+                  alt="profile"
+                />
+              ) : (
+                <RxAvatar className="w-10 h-10 rounded-full" />
+              )}
+            </div>
+
+            <button
+              className="btn"
+              onClick={async () => await authClient.signOut()}
+            >
+              Logout
+            </button>
+
+          </div>
+        ) : (
+          <div className="navbar-end flex gap-3">
+            <button className="btn">
+              <Navlink href={"/login"}>Login</Navlink>
+            </button>
+          </div>
+        )}
+
       </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-          <Navlink href={"/"}>Home</Navlink>
-    <Navlink href={"/about"}>About</Navlink>
-         <Navlink href={"/career"}>Career</Navlink>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl"></a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1 text-gray-500 font-bold flex gap-5">
-        <Navlink href={"/"}>Home</Navlink>
-    <Navlink href={"/about"}>About</Navlink>
-         <Navlink href={"/career"}>Career</Navlink>
-    </ul>
-  </div>
-  {info?
-  <div className="navbar-end flex gap-3">
-    <h2 className="font-bold text-pink-700">Wellcome {info?.name}</h2>
-    <div className="w-10 rounded-full">
-          { <img 
-  className="rounded-full w-[40px] h-[40px] object-cover"
-  src={info?.image}
-  alt="profile"
-/>||  <RxAvatar className="w-10 h-10 rounded-full"/>}
-        </div>
-
-    <button className="btn" onClick={async()=>await authClient.signOut()}>Logout </button>
-  </div>
-  :<div className="navbar-end flex gap-3">
-    <div className="w-10 rounded-full">
-          
-           
-        </div>
-
-    <button className="btn"><Navlink href={"/login"} >Login</Navlink></button>
-  </div>}
-</div>
     </div>
   );
 }
